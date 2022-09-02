e8f4db4862ab69948e497c0c8427734f8b1beb58
17hao <sqh1107@gmail.com>
2022-09-03 00:31:04 +0800

    Error handling from system calls

---
+++ err_handling.c
@@ -0,0 +1,18 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // error handling from system calls
+    int fd = open("foo", O_RDONLY);
+    if (fd == -1) {
+        if (errno == ENOENT) {
+            perror("open"); // fcntl.h open: No such file or directory
+        } else {
+            strerror(errno); // string.h
+        }
+        exit(EXIT_FAILURE);
+    }
+}
