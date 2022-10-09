b5cff2eccf92b82c24f9ce9ce86cffbac2db4c24
17hao <sqh1107@gmail.com>
2022-10-10 00:31:02 +0800

    Google style

--- err_handling.c
+++ err_handling.c
@@ -5,14 +5,14 @@
 #include <string.h>
 
 int main() {
-    // error handling from system calls
-    int fd = open("foo", O_RDONLY);
-    if (fd == -1) {
-        if (errno == ENOENT) {
-            perror("open"); // fcntl.h open: No such file or directory
-        } else {
-            strerror(errno); // string.h
-        }
-        exit(EXIT_FAILURE);
+  // error handling from system calls
+  int fd = open("foo", O_RDONLY);
+  if (fd == -1) {
+    if (errno == ENOENT) {
+      perror("open");  // fcntl.h open: No such file or directory
+    } else {
+      strerror(errno);  // string.h
     }
+    exit(EXIT_FAILURE);
+  }
 }
--- tlpi/ch04-fileio/copy.c
+++ tlpi/ch04-fileio/copy.c
@@ -11,28 +11,28 @@
 
 // ./copy copy.c /dev/tty copy a regular file to this terminal.
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("usage: %s old-file new-file\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+  if (argc != 3) {
+    printf("usage: %s old-file new-file\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-    // 1. open source file
-    int inputFd = open(argv[1], O_RDONLY);
+  // 1. open source file
+  int inputFd = open(argv[1], O_RDONLY);
 
-    // 2. create and open destination file
-    int openFlags = O_WRONLY | O_CREAT;
-    int filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; // 644
-    int outputFd = open(argv[2], openFlags, filePerms);
+  // 2. create and open destination file
+  int openFlags = O_WRONLY | O_CREAT;
+  int filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;  // 644
+  int outputFd = open(argv[2], openFlags, filePerms);
 
-    // 3. transfer data until we encounter the end of the source file
-    char buf[BUFSIZE];
-    int numRead;
-    while ((numRead = read(inputFd, buf, BUFSIZ)) > 0) {
-        write(outputFd, buf, numRead);
-    }
+  // 3. transfer data until we encounter the end of the source file
+  char buf[BUFSIZE];
+  int numRead;
+  while ((numRead = read(inputFd, buf, BUFSIZ)) > 0) {
+    write(outputFd, buf, numRead);
+  }
 
-    // 4. close files
-    close(inputFd);
-    close(outputFd);
-    exit(EXIT_SUCCESS);
+  // 4. close files
+  close(inputFd);
+  close(outputFd);
+  exit(EXIT_SUCCESS);
 }
