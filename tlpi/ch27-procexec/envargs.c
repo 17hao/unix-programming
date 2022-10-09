69a4665a80ba8d5c1a8b3cd058e3d5f0632e4a8e
17hao <sqh1107@gmail.com>
2022-10-10 00:29:41 +0800

    Print environment and args

---
+++ tlpi/ch27-procexec/envargs.c
@@ -0,0 +1,15 @@
+// Display environment and argument list.
+
+#include <stdio.h>
+
+extern char **environ;
+
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d]=%s\n", i, argv[i]);
+  }
+
+  for (char **env = environ; *env != NULL; env++) {
+    printf("environ: %s\n", *env);
+  }
+}
