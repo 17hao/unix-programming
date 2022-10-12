f38e8cdaaf8b613ce17b96fea1e1417f27e13ecd
17hao <sqh1107@gmail.com>
2022-10-12 22:41:55 +0800

    Demo of execve

---
+++ tlpi/ch27-procexec/t_execve.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// Usage: ./t_execve ./envargs
+// execve: v means argv, e means environ
+int main(int argc, char *argv[]) {
+  char *envVec[10];
+  envVec[0] = "k1=v1";
+  envVec[1] = "k2=v2";
+  envVec[2] = NULL;
+
+  char *argVec[10];
+  argVec[0] = strchr(argv[1], '/');
+  if (argVec[0] != NULL) {
+    argVec[0]++;
+  }
+  argVec[1] = "arg1";
+  argVec[2] = "arg2";
+  argVec[3] = NULL;
+
+  execve(argv[1], argVec, envVec);
+  exit(EXIT_SUCCESS);
+}
