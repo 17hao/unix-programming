fbccd8b261c53820c8229cfc1c8237e6d0cd672b
17hao <sqh1107@gmail.com>
2022-10-20 22:48:31 +0800

    kr c 5.4

---
+++ kr_c/ch5/alloc.c
@@ -0,0 +1,23 @@
+// secion 5.4
+
+#define BUF_SIZE 1024
+
+static char allocbuf[BUF_SIZE];  // storage for alloc
+static char *allocp;             // next free position
+
+// return pointer to n characters
+char *alloc(int n) {
+  if (allocbuf + BUF_SIZE - allocp >= n) {
+    allocp += n;
+    return allocp - n;
+  } else {
+    return 0;
+  }
+}
+
+// free storage pointed to by p
+void afree(char *p) {
+  if (p >= allocbuf && p <= allocbuf + BUF_SIZE) {
+    allocp = p;
+  }
+}
