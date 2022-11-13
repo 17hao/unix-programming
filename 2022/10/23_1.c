1c4f38068d46fc2b1d3c5fbd7d9c1499d4e9c971
17hao <sqh1107@gmail.com>
2022-11-13 12:49:08 +0800

    Update

--- 2022/10/23_1.c
+++ 2022/10/23_1.c
@@ -13,9 +13,8 @@ extern char **environ;  // defined in glibc posix/environ.c
 // the function putenv and the extern environ are POSIX-specific
 int main(int argc, char **argv) {
   putenv("k1=v1");
-  char **s = environ;
-  for (; *s != NULL; s++) {
-    printf("%s\n", *s);
+  for (char **tmp = environ; *tmp != NULL; tmp++) {
+    printf("%s\n", *tmp);
   }
 
   printf("==== getenv ====\n");
