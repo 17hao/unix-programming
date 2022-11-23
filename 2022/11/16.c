01b7369e60bab083e1ca6153be32ff7c91a96449
17hao <sqh1107@gmail.com>
2022-11-23 22:26:20 +0800

    array initial value

--- 2022/11/16.c
+++ 2022/11/16.c
@@ -10,7 +10,8 @@ int main(void) {
   printf("before memset: str=%s\n", str);
 
   // char str2[x / y] = {0}; compile error!
-  char str2[5] = {0};
+  // char str2[5] = {0};
+  char str2[5];
   printf("str2=%s\n", str2);
 
   int arr2D[3][4];
@@ -19,5 +20,12 @@ int main(void) {
   printf("arr2D[0]: ");
   for (int i = 0; i < 4; i++) {
     printf("%d ", arr2D[0][i]);
+  }
+  printf("\n");
+  int arr2[4];
+  memcpy(arr2, arr2D[0], sizeof(arr2));
+  printf("arr2: ");
+  for (int i = 0; i < 4; i++) {
+    printf("%d ", arr2[i]);
   }
 }
