f9078b0a299c1c7703cf01991df8c9de37b6b390
17hao <sqh1107@gmail.com>
2022-12-14 23:33:33 +0800

    mmcat.c

---
+++ tlpi/ch49-mmap/mmcat.c
@@ -0,0 +1,42 @@
+// Simple version of cat(1)
+// p1023
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    exit(EXIT_FAILURE);
+  }
+
+  int fd;
+  if ((fd = open(argv[1], O_RDONLY)) < 0) {
+    fprintf(stderr, "%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  struct stat st;
+  if (fstat(fd, &st) < 0) {
+    fprintf(stderr, "%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  char *addr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (addr == MAP_FAILED) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (write(STDOUT_FILENO, addr, st.st_size) < 0) {
+    fprintf(stderr, "%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  exit(EXIT_SUCCESS);
+}
