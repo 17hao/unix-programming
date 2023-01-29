529ede86cabb4793cae9698a1d0a1968d3cec7fc
17hao <sqh1107@gmail.com>
2023-01-29 22:14:19 +0800

    simple_thread.c

---
+++ tlpi/ch29-threads/simple_thread.c
@@ -0,0 +1,28 @@
+// volume1 p626
+// Creating a new thread and join with it.
+
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *threadFunc(void *args) {
+  char *s = (char *)args;
+  printf("[threadID=%ld] %s\n", pthread_self(), s);
+  return (void *)strlen(s);
+}
+
+int main(void) {
+  pthread_t thread;
+  int s = pthread_create(&thread, NULL, threadFunc, "Hello, world!");
+  if (s < 0) {
+    // TODO errno
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Message from main thread\n");
+
+  void *res;
+  pthread_join(thread, &res);
+  printf("Thread returned %ld\n", (long) res);
+}
