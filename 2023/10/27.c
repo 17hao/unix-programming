3f14e41e98fcc7378e0c7d5a31ffa7eedd3bffd4
17hao <sqh1107@gmail.com>
2023-10-28 00:05:37 +0800

    getcwd, opendir, readdir

---
+++ 2023/10/27.c
@@ -0,0 +1,33 @@
+#include <dirent.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+  // current wd
+  char *buf = malloc(PATH_MAX);
+  char *wd = getcwd(buf, PATH_MAX);
+  printf("current work dir is %s\n", wd);
+
+  // list files in wd
+  DIR *currentDir = opendir(wd);
+  if (errno != 0) {
+    printf("errno=%d\n", errno);
+    exit(EXIT_FAILURE);
+  }
+  printf("%s\n", wd);
+  for (;;) {
+    struct dirent *e = readdir(currentDir);
+    if (e == NULL) {
+      break;
+    }
+    if (strcmp(e->d_name, ".") == 0 || strcmp(e->d_name, "..") == 0) {
+      continue;
+    }
+    printf("|-- %s\n", e->d_name);
+  }
+}
